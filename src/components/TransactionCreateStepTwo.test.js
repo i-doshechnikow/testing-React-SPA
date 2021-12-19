5bd70746992c125887dcb673ca5985bc451f8a6c
i-doshechnikow <iliya.yurievich@yandex.ru>
2021-12-19 16:54:34 +0300

    add test for enabled button when user add value to inputs

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
 test("on initial render, the pay button is disable", async () => {
@@ -7,4 +8,13 @@ test("on initial render, the pay button is disable", async () => {
   //   screen.getByRole(""); //for see categories on page
   expect(await screen.findByRole("button", { name: "Pay" })).toBeDisabled();
   //   expect(await screen.findByRole("button", { name: "Pay" })).toBeEnabled();
+});
+
+test("if an amount and note is entered, the pay button becomes enabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ sender: "5" }} receiver={{ id: "2" }} />);
+  
+  userEvent.type(screen.getByPlaceholderText("Amount"), "50");
+  userEvent.type(screen.getByPlaceholderText("Add a note"), "dinner");
+
+  expect(await screen.findByRole("button", { name: "Pay" })).toBeEnabled();
 });
