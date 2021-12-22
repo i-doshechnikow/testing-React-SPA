0366aa1de66c8075c78327a7a870096a3bf00490
i-doshechnikow <iliya.yurievich@yandex.ru>
2021-12-22 22:57:59 +0300

    add cypress library and start make integration test

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,21 @@
+describe("payment", () => {
+  it("user can make payment", () => {
+    // login
+    cy.visit("/");
+    cy.findByRole("textbox", { name: /username/i }).type("johndoe");
+    cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+
+    cy.findByRole("button", { name: /sign in/i }).click();
+
+    // check account balance
+    // click on pay button
+    //search for user
+    // add amount and note and click pay
+    //return to transactions
+    // go to personal payments
+    // click on payment
+    // verify if payment was made
+    // verify if payment amount was deducted
+  });
+});
