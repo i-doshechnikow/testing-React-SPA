65e9ead4dd746ce0aa5a44f787c9b0c5a0578de2
i-doshechnikow <iliya.yurievich@yandex.ru>
2021-12-23 12:17:00 +0300

    completed the integration test

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -1,3 +1,6 @@
+//yarn run cypress open
+const { v4: uuidv4 } = require("uuid");
+
 describe("payment", () => {
   it("user can make payment", () => {
     // login
@@ -8,13 +11,41 @@ describe("payment", () => {
     cy.findByRole("button", { name: /sign in/i }).click();
 
     // check account balance
+    let oldBalance;
+    cy.get('[data-test="sidenav-user-balance"]').then(($balance) => (oldBalance = $balance.text()));
+
     // click on pay button
+    cy.get('[data-test="nav-top-new-transaction"]').click();
+
     //search for user
+    cy.findByRole("textbox").type("devon becker");
+    cy.findByText(/devon becker/i).click();
+
     // add amount and note and click pay
+    const payAmount = "30.00";
+    cy.get("#amount").type(payAmount);
+    let aboutAmmount = uuidv4();
+    cy.get("#transaction-create-description-input").type(aboutAmmount);
+    cy.get('[data-test="transaction-create-submit-payment"]').click();
+
     //return to transactions
+    cy.get('[data-test="new-transaction-return-to-transactions"]').click();
+
     // go to personal payments
+    cy.get('[data-test="nav-personal-tab"]').click();
+
     // click on payment
+    cy.findByText(aboutAmmount).click({ force: true });
+
     // verify if payment was made
+    cy.findByText(`-$${payAmount}`).should("be.visible");
+    cy.findByText(aboutAmmount).should("be.visible");
+
     // verify if payment amount was deducted
+    cy.get('[data-test="sidenav-user-balance"]').then(($balance) => {
+      const convertedOldBalance = parseFloat(oldBalance.replace(/\$|,/g, ""));
+      const convertedNewBalance = parseFloat($balance.text().replace(/\$|,/g, ""));
+      expect(convertedOldBalance - convertedNewBalance).to.equal(parseFloat(payAmount));
+    });
   });
 });
